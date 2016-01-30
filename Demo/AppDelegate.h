7ef7304264fcad750c7e29e075aa54be2224179b
wenbegn <wenbegn@wenbegndemacbook-air.local>
2016-01-30 15:30:47 +0800

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by wenbegn on 16/1/30.
+//  Copyright © 2016年 wenbegn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by wenbegn on 16/1/30.
+//  Copyright © 2016年 wenbegn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
